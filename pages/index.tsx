0c366041ab4fa1cd5023d1f99be7c8e2fbb00f60
LGE0131 <igoeun0131@gamil.com>
2022-05-22 12:57:06 +0900

    add Nav component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,11 @@ import type { NextPage } from "next";
 import Nav from "../components/Nav";
 
 const Home: NextPage = () => {
-  return <div>Nav</div>;
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
 };
 
 export default Home;
